670beabc0dead0353b97931285734508be5a8954
TomGB <tgbanister@gmail.com>
2016-02-29 22:41:47 +0000

    working on file saving

---
+++ platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/Users/tgbmac/Documents/my_projects/Be Happy/platforms/android/CordovaLib/ant-build/AndroidManifest.xml : \
+/Users/tgbmac/Documents/my_projects/Be Happy/platforms/android/CordovaLib/AndroidManifest.xml \
---
+++ platforms/android/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/Users/tgbmac/Documents/my_projects/Be Happy/platforms/android/ant-build/AndroidManifest.xml : \
+/Users/tgbmac/Documents/my_projects/Be Happy/platforms/android/AndroidManifest.xml \
